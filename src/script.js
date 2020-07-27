e18fb3eaf53d3d92c5beb99fc0d26d105da357d2
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2020-07-27 14:17:14 +0100

    switch to component system

--- src/script.js
+++ src/script.js
@@ -1,68 +1,14 @@
-const navbarHTML = `
-<nav class="navbar navbar has-shadow is-spaced">
-  <div class="container">
-    <div class="navbar-brand">
-      <a role="button" onclick="toggleClass(this, 'navMenu')" class="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div class="navbar-menu" id="navMenu">
-      <div class="navbar-start">
-        <a class="navbar-item" href="index.html">
-          <span class="icon has-text-primary">
-            <span class="fa fa-home"></span>
-          </span>
-          <span>Home</span>
-        </a>
-        <a class="navbar-item" href="research.html">
-          <span class="icon has-text-primary">
-            <span class="fa fa-book"></span>
-          </span>
-          <span>Research</span>
-        </a>
-        <a class="navbar-item" href="code.html">
-          <span class="icon has-text-primary">
-            <span class="fa fa-code"></span>
-          </span>
-          <span>Code</span>
-        </a>
-      </div>
-      <div class="navbar-end">
-        <a class="navbar-item" href="https://piever.github.io/simpleblog/">
-          <span class="icon has-text-primary">
-            <span class="fa fa-rss"></span>
-          </span>
-          <span>Blog</span>
-        </a>
-        <a class="navbar-item" href="https://github.com/piever">
-          <span class="icon has-text-primary">
-            <span class="fab fa-github"></span>
-          </span>
-          <span>Github</span>
-        </a>
-      </div>
-    </div>
-  </div>
-</nav>
-`
+async function loadTemplate(tag) {
+  var request = await fetch('templates/' + tag + '.html')
+  for (let content of document.getElementsByTagName(tag))
+    {
+      content.outerHTML = await request.text();
+    }
+}
 
-const navbars = document.getElementsByTagName('navbar');
-for (let el of navbars) { el.outerHTML = navbarHTML;}
+const tags = ['navbar', 'footer', 'home', 'research', 'code']
 
-const footerHTML = `
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      Website created with <strong>Bulma</strong>.
-    </p>
-  </div>
-</footer>
-`
-
-const footers = document.getElementsByTagName('footer');
-for (let el of footers) { el.outerHTML = footerHTML;}
+tags.forEach(loadTemplate)
 
 function toggleClass(el, target) {
   el.classList.toggle('is-active');
